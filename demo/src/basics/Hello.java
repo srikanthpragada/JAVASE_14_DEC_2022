b4833ae010046fe219e53cb8908330941c7cd948
srikanthpragada <srikanthpragada@gmail.com>
2022-12-17 16:37:58 +0530

    17-DEC

---
+++ demo/src/basics/Hello.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		  System.out.println("Hello Java");
+
+	}
+
+}
---
+++ demo/src/basics/Numbers.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		int i;
+
+		for (i = 1; i <= 10; i++)
+			System.out.println(i);
+
+	}
+
+}
