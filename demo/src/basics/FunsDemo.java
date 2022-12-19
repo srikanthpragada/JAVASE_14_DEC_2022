428b8d115d84eec48be1da710e4000441a7d5409
srikanthpragada <srikanthpragada@gmail.com>
2022-12-19 20:57:38 +0530

    19-DEC

--- demo/src/basics/FunsDemo.java
+++ demo/src/basics/FunsDemo.java
@@ -5,9 +5,16 @@ public class FunsDemo {
 	static int add(int a, int b) {
 		return a + b;
 	}
+
+	static void process(int ... values) {
+
+	}
+
 	public static void main(String[] args) {
-		 
-        System.out.println(add(10,20));
+
+		System.out.println(add(10, 20));
+		process(10, 20);
+		process(10, 20, 30, 40);
 	}
 
 }
